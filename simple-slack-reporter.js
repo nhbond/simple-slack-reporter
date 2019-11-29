dd952fe86d40eede9416f1556b71c0f9c5bff936
nhbond <>
2019-11-29 14:49:00 +1300

    include passing tests in output

--- simple-slack-reporter.js
+++ simple-slack-reporter.js
@@ -5,7 +5,10 @@
       const slack = new NodeSlack(options.reporterOptions.hook_url);
       var errors = '';
       runner.on("fail", function (test, err) {
-        errors+=`*${test.parent.parent.title} ${test.parent.title} ${test.title}*\n>${err}\n`;
+        errors+=`*_FAIL - _* *${test.parent.parent.title} ${test.parent.title} ${test.title}*\n>${err}\n`;
+      });
+      runner.on("pass", function (test, err) {
+        errors+=`*_PASS - _* *${test.parent.parent.title} ${test.parent.title} ${test.title}*\n>${err?err:''}\n`;
       });
       runner.once("end", function () {
         slack.send({text: errors});
