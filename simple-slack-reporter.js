6b9b7b01ee2b10df2267f869b8df78c910a09906
nhbond <>
2019-11-29 15:50:25 +1300

    add minimal flag

--- simple-slack-reporter.js
+++ simple-slack-reporter.js
@@ -5,11 +5,14 @@
       const slack = new NodeSlack(options.reporterOptions.hook_url);
       var errors = '';
       runner.on("fail", function (test, err) {
-        errors+=`*_FAIL - _* *${test.parent.parent.title} ${test.parent.title} ${test.title}*\n>${err}\n`;
+        errors+=`*_FAIL_ - * *${test.parent.parent.title} ${test.parent.title} ${test.title}*\n>${err}\n`;
       });
-      runner.on("pass", function (test, err) {
-        errors+=`*_PASS - _* *${test.parent.parent.title} ${test.parent.title} ${test.title}*\n>${err?err:''}\n`;
-      });
+      // evaluate minimal as both boolean and string => reporter-options entered on cli register as strings
+      if (!options.reporterOptions.minimal || options.reporterOptions.minimal == 'false') {
+        runner.on("pass", function (test, err) {
+          errors+=`*_PASS_ - * *${test.parent.parent.title} ${test.parent.title} ${test.title}*\n>${err?err:''}\n`;
+        });
+      }
       runner.once("end", function () {
         slack.send({text: errors});
       });
